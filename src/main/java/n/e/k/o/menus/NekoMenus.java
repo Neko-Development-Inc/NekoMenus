bb648e2b92a277c54f88dee701c61fd2c70b189b
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2023-01-28 14:26:36 +0100

    Added server-sided only

--- src/main/java/n/e/k/o/menus/NekoMenus.java
+++ src/main/java/n/e/k/o/menus/NekoMenus.java
@@ -3,8 +3,12 @@ package n.e.k.o.menus;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -20,6 +24,7 @@ public class NekoMenus {
 
     public NekoMenus() {
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 
     @SubscribeEvent
