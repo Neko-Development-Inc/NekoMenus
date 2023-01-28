d49d094af2b3cf906464e7a814f11cf8d8f42b01
HoverCatz <1442391+HoverCatz@users.noreply.github.com>
2023-01-28 14:26:39 +0100

    Update StringColorUtils.java

--- src/main/java/n/e/k/o/menus/utils/StringColorUtils.java
+++ src/main/java/n/e/k/o/menus/utils/StringColorUtils.java
@@ -5,6 +5,18 @@ import net.minecraft.util.text.StringTextComponent;
 
 public class StringColorUtils {
 
+    // &([\da-fk-or])
+    //            ^ r
+    //         ^-^ from k to o
+    //      ^-^ from a to f
+    //    ^^ number 0-9
+    //   ^ expect one char
+    //  ^ first group
+    // ^ &
+    //
+    // s&0s, &9, &a, &f, &k, &o, &r
+    // s§0s, §9, §a, §f, §k, §o, §r
+
     public static IFormattableTextComponent getColoredString(String string) {
         return new StringTextComponent((string == null ? "" : string.replaceAll("&([\\da-fk-or])", "§$1")));
     }
